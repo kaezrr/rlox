9d47afbde20ffa4e08798c6a4e0945950b57f054
Anjishnu Banerjee <inviktor27@gmail.com>
2026-02-20 21:51:29 +0530

    Remove quotes from string prints

--- rlox/src/token.rs
+++ rlox/src/token.rs
@@ -365,7 +365,7 @@ impl Display for Literal {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Literal::Number(v) => write!(f, "{}", v),
-            Literal::String(v) => write!(f, "\"{}\"", v),
+            Literal::String(v) => write!(f, "{}", v),
             Literal::Boolean(v) => write!(f, "{}", v),
             Literal::Nil => write!(f, "nil"),
             Literal::Callable(v) => write!(f, "{}", v.name),
